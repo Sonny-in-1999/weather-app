31f06753de10edbcf05202c3a1576ce7e695d188
minsu <minsoo1022@gmail.com>
2023-09-29 23:13:02 +0900

    시간 포맷팅 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,11 @@ export default async function Home() {
     const time = await getTime(res.location.tz_id);
 
     // 날짜 및 시간 포맷팅 함수
-    const formatDateTime = (dateTime: String) => {
-        const date = new Date(dateTime);
-        return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
-    };
 
     console.log(res);
     return <>
         <h1>날씨 앱</h1>
-        <h3>{formatDateTime(time.dateTime)}</h3>
+        <h3>{time.dateTime}</h3>
         <ul className={style.list}>
             <li>
                 <Link href="/seoul?name=서울">서울</Link>
